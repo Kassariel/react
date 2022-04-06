fd7ec3ecd6f2cb90c6cde73f9ac7bdd6b82f330f
Kassariel <102538405+Kassariel@users.noreply.github.com>
2022-04-06 22:14:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Example from './Example';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div 
+      className={"App"}
+      style={{top: props.topPosition || '20px', position: 'relative'}}>
+      <header 
+      className={`App-header ${props.showRed ? 'header-red' : 'header-blue'}`}>
+        Hello, {props.name}
+      
+      <Example />
       </header>
     </div>
   );
